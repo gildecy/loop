d1716c94c0b654b9a49dd7887a7c7c10e5012291
Gildecy de Jesus Sá <101933897+gildecy@users.noreply.github.com>
2022-09-24 13:03:03 -0300

    Add files via upload
    conhecendo tipo de loop

---
+++ tiposde-loop.js
@@ -0,0 +1,18 @@
+// exemplos de loop 
+
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
+// o loop acima irá iniciar o contador do 0 até número 10
+
+
+// ultilizando condicional e break no loop  
+
+for (let i = 10; i > 0; i++) {
+    if (i === 5) {
+        break;
+    }
+}
+console.log(i)
+
+// o loop acima ira para o contador quando a contagem chegar no número 5
